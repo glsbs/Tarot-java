16eb89d499ffd479aecff01148b0c4f708649803
glsbs <gulsubese@gmail.com>
2025-04-07 15:55:07 +0300

    Update Test.java

--- JavaApplication74/src/javaapplication74/Test.java
+++ JavaApplication74/src/javaapplication74/Test.java
@@ -6,13 +6,11 @@ package javaapplication74;
 
 import java.util.Scanner;
 
-public class Test{
+public class Main{
 
     public static void main(String[] args) {
         
         Scanner klavye = new Scanner(System.in);
-      // User u= new User();
-        //u.kategoriCagirma();
         System.out.println("Merhaba");
         User u2=new User("gulsu");
         u2.kategoriCagirma();
